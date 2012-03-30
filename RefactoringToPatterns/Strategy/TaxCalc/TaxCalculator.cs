9c1e3fa548a158ef1b8a3371247d1a3d48523b57
zhuanqs <p4ssw0rd@163.com>
2012-03-30 22:36:17 +0800

    extract method for duplicated logic

--- RefactoringToPatterns/Strategy/TaxCalc/TaxCalculator.cs
+++ RefactoringToPatterns/Strategy/TaxCalc/TaxCalculator.cs
@@ -13,25 +13,45 @@
 
             if(taxbase <= 1500)
             {
-                return taxbase * 0.03;
+                return CalcFor0To1500(taxbase);
             }
 
             if (taxbase <= 4500)
             {
-                return 1500*0.03 + (taxbase - 1500)*0.1;
+                return Calc1500To4500(taxbase);
             }
 
             if(taxbase <= 9000)
             {
-                return 1500*0.03 + (4500 - 1500)*0.1 + (taxbase - 4500)*0.20;
+                return Calc4500To9000(taxbase);
             }
 
             if (taxbase <= 35000)
             {
-                return 1500*0.03 + (4500 - 1500)*0.1 + (9000 - 4500)*0.2 + (taxbase - 9000)*0.25;
+                return Calc9000To35000(taxbase);
             }
 
             return 0;
+        }
+
+        private static double Calc9000To35000(double taxbase)
+        {
+            return Calc4500To9000(9000 - 4500) + (taxbase - 9000)*0.25;
+        }
+
+        private static double Calc4500To9000(double taxbase)
+        {
+            return Calc1500To4500(4500-1500) + (taxbase - 4500)*0.20;
+        }
+
+        private static double Calc1500To4500(double taxbase)
+        {
+            return CalcFor0To1500(1500) + (taxbase - 1500)*0.1;
+        }
+
+        private static double CalcFor0To1500(double taxbase)
+        {
+            return taxbase * 0.03;
         }
     }
 }
